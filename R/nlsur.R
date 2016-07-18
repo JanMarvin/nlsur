79e00de90b3f46aa404da23dd743badfe263469b
Marvin <jmg@edna>
2016-07-18 13:39:43 +0200

    * missed this one

--- R/nlsur.R
+++ R/nlsur.R
@@ -65,7 +65,7 @@
 #' Gallant, A. Ronald (1987): Nonlinear Statistical Models. Wiley: New York
 #' @importFrom Matrix diag kronecker rankMatrix
 #' @importFrom MASS lm.gls
-#' @importFrom parallel mclapply
+#' @importFrom parallel mcmapply mclapply
 #' @importFrom stats as.formula coef deriv
 #' @import RcppArmadillo
 #' @useDynLib nlsur
